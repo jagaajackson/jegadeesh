f02f9f05eca7863ae8871d8cca42a0eb8333213f
jagaajackson <jagaa2810@gmail.com>
2017-05-03 11:48:12 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,10 @@
+public class student {
+   public student(String name) {
+     
+      System.out.println("Passed Name is :" + name );
+   }
+
+   public static void main(String []args) {
+      student mystudent = new student( "jaga" );
+   }
+}
